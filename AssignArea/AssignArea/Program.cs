79f181c4e26337099301f0517582a074a42d94af
Akshay Pimpalkar <akshay.pimpalkar@gmail.com>
2017-06-06 14:45:27 +0530

    area of shape with method area

---
+++ AssignArea/AssignArea/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignArea
+{
+    class Shape
+    {
+        readonly int _height;
+        int _width;
+
+        public Shape(int w)
+        {
+            _height = 20;
+            _width = w;
+        }
+
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+            }
+        }
+
+        public int area()
+        {
+            return Width * Height;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s = new Shape(25);
+            Console.WriteLine("Area of the shape is {0}", s.area());
+        }
+    }
+}
