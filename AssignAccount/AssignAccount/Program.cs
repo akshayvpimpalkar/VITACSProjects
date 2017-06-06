05fd256b8e151b84f254688e467a3ca4f99aafa4
Akshay Pimpalkar <akshay.pimpalkar@gmail.com>
2017-06-06 10:32:45 +0530

    with dposit and display method

---
+++ AssignAccount/AssignAccount/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignAccount
+{
+    class Account
+    {
+        string name;
+        float balance;
+
+        public Account(string name, float balance)
+        {
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public void deposit(int amt)
+        {
+            this.balance += (float)amt;
+        }
+        public void display()
+        {
+            Console.WriteLine("Balance for {0} after deposit is : {1}", name, balance);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Tim", 75000.00f);
+            a1.deposit(2000);
+            a1.display();
+
+            Account a2 = new Account("Nick", 36000.0f);
+            a2.deposit(7000);
+            a2.display();
+
+            Account a3 = new Account("Jessy", 42000.0f);
+            a3.deposit(4000);
+            a3.display();
+        }
+    }
+}
--- SampleApp/SampleApp/Program.cs
+++ SampleApp/SampleApp/Program.cs
@@ -11,6 +11,7 @@ namespace SampleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Guys!");
+            Console.WriteLine("Second line");
         }
     }
 }
